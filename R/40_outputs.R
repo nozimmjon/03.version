f977fb32a2a7055833b6cd82c48ce67f8e7544b7
nozimmjon <noz4ik@gmail.com>
2026-02-25 18:36:50 +0500

    clearing the old codes and files

--- R/40_outputs.R
+++
@@ -1,8 +0,0 @@
-# 40_outputs.R
-source(here::here("R/00_setup.R"))
-
-save_gt <- function(tbl, path) {
-  dir.create(dirname(path), recursive = TRUE, showWarnings = FALSE)
-  gt::gtsave(tbl, path)
-  invisible(path)
-}
